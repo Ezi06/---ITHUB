4315993864f68dd441a420bbb96c96b528625d94
Islam <islam@islams-macbook-pro.local>
2018-11-06 14:17:52 +0300

    Initial Commit

---
+++ визитка/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  визитка
+//
+//  Created by Islam Dzhabrailov on 11/6/18.
+//  Copyright © 2018 Islam Dzhabrailov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
